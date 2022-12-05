9e3dec3744f3349b7e1c9d23d84870498097c080
Vkakz <36102806+Vinay-Kakkar@users.noreply.github.com>
2022-12-05 17:42:15 +0000

    Cleaning Up Code

--- Proof of Concepts/PDB onto a Cluster/PDBontoaCluster.py
+++ Proof of Concepts/PDB onto a Cluster/PDBontoaCluster.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType,StructField, StringType, IntegerType
+from pyspark.sql.types import StringType
 import os
 
 def getallpdbfiles(directory):
@@ -10,28 +10,40 @@ def getallpdbfiles(directory):
             pdbfiles.append(filename)
     return pdbfiles
 
+def listallpdbfiles(pdbfiles):
+    pdbs = []
+    for pdbfile in pdbfiles:
+        filepath = (directory.decode('utf-8')+"/"+pdbfile)
+        pdbs.append(readPDBfile(filepath))
+    return pdbs
+
+def convertPDBintoRDD(pdbs):
+    rdds = []
+    for pdb in pdbs:
+        rdds.append(pdb.rdd.flatMap(lambda x: list(x)))
+    return rdds
+
+def convertrddsintodfs(rdds):
+    dfs = []
+    for rdd in rdds:
+        dfs.append(spark.createDataFrame(rdd, StringType()))
+    return dfs
+
 def readPDBfile(fileloc):
     pdb = spark.read.option('header','true').csv(fileloc, inferSchema = True)
     return pdb
 
 spark = SparkSession.builder.appName('PDB').getOrCreate()
-
 directory = os.fsencode("Proof of Concepts/PDB onto a Cluster/Original PDBs")
+
 pdbfiles = getallpdbfiles(directory)
 
-pdbs = []
-for pdbfile in pdbfiles:
-    filepath = (directory.decode('utf-8')+"/"+pdbfile)
-    pdbs.append(readPDBfile(filepath))
 
-rdds = []
-for pdb in pdbs:
-    rdds.append(pdb.rdd.flatMap(lambda x: list(x)))
+pdbs = listallpdbfiles(pdbfiles)
 
+rdds = convertPDBintoRDD(pdbs)
 
-dfs = []
-for rdd in rdds:
-    dfs.append(spark.createDataFrame(rdd, StringType()))
+dfs = convertrddsintodfs(rdds)
 
 
 for df in dfs:
