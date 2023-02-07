d96fd8395d521c3c49acbd4738489a203d685ea9
Vkakz <36102806+Vinay-Kakkar@users.noreply.github.com>
2023-02-07 20:29:18 +0000

    New Program Using key value pairs

---
+++ Programs/Lines/LinesUsingKeyValuePairs.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType
+import os
+import shutil
+from pathlib import Path
+import time
+
+
+def main(spark):
+    start = time.time()
+    directory = ("/Users/vinaykakkar/Desktop/PROJECT/Programs/Lines/PDBsDirectory/*")
+    tempdirectory = "/Users/vinaykakkar/Desktop/PROJECT/Programs/Lines/PDBsfromRDD"
+
+    rddkeyvalue = spark.sparkContext.wholeTextFiles(directory)
+
+    def numberoflinesinfile(k):
+        os.system("wc -l "+ k[40:])
+    
+    rddkeyvalue.map(lambda x: numberoflinesinfile(x[0])).collect()
+
+    end = time.time()
+    print(end - start)
+
+
+if __name__ == '__main__':
+    # This creates a local cluster
+    spark = SparkSession.builder.appName('PDB').getOrCreate()
+    main(spark)
+
+
