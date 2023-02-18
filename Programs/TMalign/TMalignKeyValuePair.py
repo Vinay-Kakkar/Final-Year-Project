2819b84b76f16189e7f6bd75e79c491b4cc2f65e
Vkakz <36102806+Vinay-Kakkar@users.noreply.github.com>
2023-02-18 14:58:16 +0000

    Also improved TMalign to use keyValue pairs

--- Programs/TMalign/TMalignKeyValuePair.py
+++ Programs/TMalign/TMalignKeyValuePair.py
@@ -5,7 +5,7 @@ import shutil
 from pathlib import Path
 import time
 
-
+# Need to run this file in the correct location in the terminal to work
 def main(spark):
     start = time.time()
     directory1 = ("/Users/vinaykakkar/Desktop/PROJECT/Programs/TMalign/PDBsDirectory1/*")
@@ -14,21 +14,16 @@ def main(spark):
     rddkeyvalue1 = spark.sparkContext.wholeTextFiles(directory1)
     rddkeyvalue2 = spark.sparkContext.wholeTextFiles(directory2)
 
-    lis = []
+    keys1 = rddkeyvalue1.keys()
+    keys2 = rddkeyvalue2.keys()
 
-    def getNextKey(y):
-        rdd.map(lambda x: runTmAlign(x[0], y)).collect()
+    rdd = keys1.cartesian(keys2)
 
+    def runTMalign(tuple):
+        print("./TMalign " + tuple[0][57:] + " " + tuple[1][57:])
+        os.system("./TMalign " + tuple[0][57:] + " " + tuple[1][57:])
 
-    def runTmAlign(x, y):
-        os.system("./TMalign " + x + " " + y)
-
-    rdd = rddkeyvalue1.union(rddkeyvalue2).groupByKey()
-
-    rdd.map(lambda x: getNextKey(x[0])).collect()
-
-
-    print(lis)
+    rdd.map(lambda tuple: runTMalign(tuple)).collect()
         
     end = time.time()
     print(end - start)
@@ -40,3 +35,4 @@ if __name__ == '__main__':
     main(spark)
 
 
+# If in the case we can not hold files in the hdfs we will need to cartesian each the rdd values then have the function create the file then run the tmalign function
