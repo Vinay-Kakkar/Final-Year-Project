c8c83b33846abe05d71356648936a05a422b5bf7
Vkakz <36102806+Vinay-Kakkar@users.noreply.github.com>
2023-03-30 19:57:46 +0100

    Docstrings to bench programs

--- main/bench_linecount.py
+++ main/bench_linecount.py
@@ -16,6 +16,20 @@ from pyspark import SparkConf, SparkContext
 
 
 def lineCount(spark, fileName):
+    """
+    Counts the number of lines in each file of a given directory using Spark.
+
+    Args:
+    - spark (SparkSession): A SparkSession object.
+    - fileName (str): A string representing the name of the directory containing the files.
+
+    Returns:
+    - bool: Returns True if the function runs successfully.
+
+    Raises:
+    - Exception: If the directory specified by 'fileName' does not exist.
+    - Exception: If no files are found in the specified directory.
+    """
     # Function to count the number of lines in a file
     # python3 main.py linecount PDBsDirectory1
     directory = ("/Users/vinaykakkar/Desktop/PROJECT/main/" + fileName + "/*")
@@ -41,61 +55,74 @@ def lineCount(spark, fileName):
 
     return True
 
+def bench():
+    """
+    Executes the lineCount function with different Spark configurations and plots a graph of execution times.
 
-conf1 = SparkConf().setAppName("MyApp").setMaster("local[1]") \
-    .set("spark.executor.cores", "1")
-spark1 = SparkContext(conf=conf1)
-t1 = time.time()
-lineCount(spark1, "OriginalPDBs")
-t1c = time.time()
+    Returns:
+        None
+    
+    Raises:
+        None    
+
+    Output:
+    - A graph of execution times by configuration.
+    """
+    conf1 = SparkConf().setAppName("MyApp").setMaster("local[1]") \
+        .set("spark.executor.cores", "1")
+    spark1 = SparkContext(conf=conf1)
+    t1 = time.time()
+    lineCount(spark1, "OriginalPDBs")
+    t1c = time.time()
 
 
-conf2 = SparkConf().setAppName("MyApp").setMaster("local[1]") \
-    .set("spark.executor.cores", "8")
-spark2 = SparkContext(conf=conf2)
-t2 = time.time()
-lineCount(spark2, "OriginalPDBs")
-t2c = time.time()
+    conf2 = SparkConf().setAppName("MyApp").setMaster("local[1]") \
+        .set("spark.executor.cores", "8")
+    spark2 = SparkContext(conf=conf2)
+    t2 = time.time()
+    lineCount(spark2, "OriginalPDBs")
+    t2c = time.time()
 
-conf3 = SparkConf().setAppName("MyApp").setMaster("local[2]") \
-    .set("spark.executor.cores", "2")
-spark3 = SparkContext(conf=conf3)
-t3 = time.time()
-lineCount(spark3, "OriginalPDBs")
-t3c = time.time()
+    conf3 = SparkConf().setAppName("MyApp").setMaster("local[2]") \
+        .set("spark.executor.cores", "2")
+    spark3 = SparkContext(conf=conf3)
+    t3 = time.time()
+    lineCount(spark3, "OriginalPDBs")
+    t3c = time.time()
 
-conf4 = SparkConf().setAppName("MyApp").setMaster("local[2]") \
-    .set("spark.executor.cores", "4")
-spark4 = SparkContext(conf=conf4)
-t4 = time.time()
-lineCount(spark4, "OriginalPDBs")
-t4c = time.time()
+    conf4 = SparkConf().setAppName("MyApp").setMaster("local[2]") \
+        .set("spark.executor.cores", "4")
+    spark4 = SparkContext(conf=conf4)
+    t4 = time.time()
+    lineCount(spark4, "OriginalPDBs")
+    t4c = time.time()
 
-conf5 = SparkConf().setAppName("MyApp").setMaster("local[4]") \
-    .set("spark.executor.cores", "2")
-spark5 = SparkContext(conf=conf5)
-t5 = time.time()
-lineCount(spark5, "OriginalPDBs")
-t5c = time.time()
+    conf5 = SparkConf().setAppName("MyApp").setMaster("local[4]") \
+        .set("spark.executor.cores", "2")
+    spark5 = SparkContext(conf=conf5)
+    t5 = time.time()
+    lineCount(spark5, "OriginalPDBs")
+    t5c = time.time()
 
-conf6 = SparkConf().setAppName("MyApp").setMaster("local[8]") \
-    .set("spark.executor.cores", "1")
-spark6 = SparkContext(conf=conf5)
-t6 = time.time()
-lineCount(spark6, "OriginalPDBs")
-t6c = time.time()
+    conf6 = SparkConf().setAppName("MyApp").setMaster("local[8]") \
+        .set("spark.executor.cores", "1")
+    spark6 = SparkContext(conf=conf5)
+    t6 = time.time()
+    lineCount(spark6, "OriginalPDBs")
+    t6c = time.time()
 
-# Create a list of execution times for different configurations
-times = [(t1c - t1), (t2c - t2), (t3c - t3), (t4c - t4), (t5c - t5), (t6c - t6)]
-# Create x axis labels
+    # Create a list of execution times for different configurations
+    times = [(t1c - t1), (t2c - t2), (t3c - t3), (t4c - t4), (t5c - t5), (t6c - t6)]
+    # Create x axis labels
 
-configurations = ["Config 1", "Config 2", "Config 3", "Config 4", "Config 5", "Config 6"]
+    configurations = ["Config 1", "Config 2", "Config 3", "Config 4", "Config 5", "Config 6"]
 
-plt.plot(configurations, times, linewidth=2, color="#1f77b4")
-plt.ylabel("Time (seconds)", fontsize=12)
-plt.xlabel("Configuration", fontsize=12)
-plt.title("Line Plot of Execution Time by Configuration", fontsize=14)
-plt.xticks(fontsize=10, rotation=45)
-plt.yticks(fontsize=10)
-plt.grid(axis="y", linestyle=":", alpha=0.7)
-plt.show()
+    plt.plot(configurations, times, linewidth=2, color="#1f77b4")
+    plt.ylabel("Time (seconds)", fontsize=12)
+    plt.xlabel("Configuration", fontsize=12)
+    plt.title("Line Plot of Execution Time by Configuration", fontsize=14)
+    plt.xticks(fontsize=10, rotation=45)
+    plt.yticks(fontsize=10)
+    plt.grid(axis="y", linestyle=":", alpha=0.7)
+    plt.show()
+bench()
--- main/bench_tmalign.py
+++ main/bench_tmalign.py
@@ -18,17 +18,45 @@ from pyspark import SparkConf, SparkContext
 
 
 def tmalign(spark, folder1, folder2):
+    """
+    Performs TM-align structural alignment of proteins in the directories 'folder1' and 'folder2'.
+    
+    Args:
+        spark (SparkSession): A SparkSession object.
+        folder1 (str): A string representing the path of the first directory containing protein PDB files.
+        folder2 (str): A string representing the path of the second directory containing protein PDB files.
+
+    Returns:
+        None
+
+    Raises:
+        Exception: If the path for either of the folders doesn't exist.
+        Exception: If there are no PDB files found in the directories.
+    """
     # python3 main.py tmalign PDBsDirectory1 PDBsDirectory2
+    if not os.path.exists(folder1):
+        raise Exception("Path does not exist")
+    if not os.path.exists(folder2):
+        raise Exception("Path does not exist")
+    
     directory1 = ("/Users/vinaykakkar/Desktop/PROJECT/main/"+folder1+"/*")
     directory2 = ("/Users/vinaykakkar/Desktop/PROJECT/main/"+folder2+"/*")
 
-    rddKeyValue1 = spark.wholeTextFiles(directory1)
-    rddKeyValue2 = spark.wholeTextFiles(directory2)
+    
+    files = os.listdir(folder1)
+    if not files:
+        raise Exception("No files found in folder provided")
+    files = os.listdir(folder1)
+    if not files:
+        raise Exception("No files found in folder provided")
+
+    rddKeyValue1 = spark.sparkContext.wholeTextFiles(directory1)
+    rddKeyValue2 = spark.sparkContext.wholeTextFiles(directory2)
 
     rdd = rddKeyValue1.cartesian(rddKeyValue2)
 
     def runTMalign(tuple1, tuple2):
-        with tempfile.NamedTemporaryFile(mode='w') as tmp1, tempfile.NamedTemporaryFile(mode='w') as tmp2:
+        with tempfile.NamedTemporaryFile(prefix = tuple1[0][60:], mode='w') as tmp1, tempfile.NamedTemporaryFile(prefix = tuple2[0][60:], mode='w') as tmp2:
             tmp1.write(tuple1[1])
             tmp2.write(tuple2[1])
             #print("./TMalign " + tmp1.name + " " + tmp2.name)
@@ -37,59 +65,72 @@ def tmalign(spark, folder1, folder2):
     rdd.map(lambda tuple: runTMalign(tuple[0], tuple[1])).collect()
     spark.stop()
 
+def bench():
+    """
+    Executes the lineCount function with different Spark configurations and plots a graph of execution times.
 
-conf1 = SparkConf().setAppName("MyApp").setMaster("local[1]") \
-    .set("spark.executor.cores", "1")
-spark1 = SparkContext(conf=conf1)
-t1 = time.time()
-tmalign(spark1, "OriginalPDBs", "OriginalPDBscopy")
-t1c = time.time()
+    Returns:
+        None
+    
+    Raises:
+        None    
 
-
-conf2 = SparkConf().setAppName("MyApp").setMaster("local[1]") \
-    .set("spark.executor.cores", "8")
-spark2 = SparkContext(conf=conf2)
-t2 = time.time()
-tmalign(spark2, "OriginalPDBs", "OriginalPDBscopy")
-t2c = time.time()
-
-conf3 = SparkConf().setAppName("MyApp").setMaster("local[2]") \
-    .set("spark.executor.cores", "2")
-spark3 = SparkContext(conf=conf3)
-t3 = time.time()
-tmalign(spark3, "OriginalPDBs", "OriginalPDBscopy")
-t3c = time.time()
-
-conf4 = SparkConf().setAppName("MyApp").setMaster("local[2]") \
-    .set("spark.executor.cores", "4")
-spark4 = SparkContext(conf=conf4)
-t4 = time.time()
-tmalign(spark4, "OriginalPDBs", "OriginalPDBscopy")
-t4c = time.time()
-
-conf5 = SparkConf().setAppName("MyApp").setMaster("local[4]") \
-    .set("spark.executor.cores", "2")
-spark5 = SparkContext(conf=conf5)
-t5 = time.time()
-tmalign(spark5, "OriginalPDBs", "OriginalPDBscopy")
-t5c = time.time()
-
-conf6 = SparkConf().setAppName("MyApp").setMaster("local[8]") \
-    .set("spark.executor.cores", "1")
-spark6 = SparkContext(conf=conf5)
-t6 = time.time()
-tmalign(spark6, "OriginalPDBs", "OriginalPDBscopy")
-t6c = time.time()
+    Output:
+    - A graph of execution times by configuration.
+    """
+    conf1 = SparkConf().setAppName("MyApp").setMaster("local[1]") \
+        .set("spark.executor.cores", "1")
+    spark1 = SparkContext(conf=conf1)
+    t1 = time.time()
+    tmalign(spark1, "OriginalPDBs", "OriginalPDBscopy")
+    t1c = time.time()
 
 
-times = [(t1c-t1), (t2c-t2), (t3c-t3), (t4c-t4), (t5c-t5), (t6c-t6)]
-configurations = ["Config 1", "Config 2", "Config 3", "Config 4", "Config 5", "Config 6"]
+    conf2 = SparkConf().setAppName("MyApp").setMaster("local[1]") \
+        .set("spark.executor.cores", "8")
+    spark2 = SparkContext(conf=conf2)
+    t2 = time.time()
+    tmalign(spark2, "OriginalPDBs", "OriginalPDBscopy")
+    t2c = time.time()
 
-plt.plot(configurations, times, linewidth=2, color="#1f77b4")
-plt.ylabel("Time (seconds)", fontsize=12)
-plt.xlabel("Configuration", fontsize=12)
-plt.title("Line Plot of Execution Time by Configuration", fontsize=14)
-plt.xticks(fontsize=10, rotation=45)
-plt.yticks(fontsize=10)
-plt.grid(axis="y", linestyle=":", alpha=0.7)
-plt.show()
+    conf3 = SparkConf().setAppName("MyApp").setMaster("local[2]") \
+        .set("spark.executor.cores", "2")
+    spark3 = SparkContext(conf=conf3)
+    t3 = time.time()
+    tmalign(spark3, "OriginalPDBs", "OriginalPDBscopy")
+    t3c = time.time()
+
+    conf4 = SparkConf().setAppName("MyApp").setMaster("local[2]") \
+        .set("spark.executor.cores", "4")
+    spark4 = SparkContext(conf=conf4)
+    t4 = time.time()
+    tmalign(spark4, "OriginalPDBs", "OriginalPDBscopy")
+    t4c = time.time()
+
+    conf5 = SparkConf().setAppName("MyApp").setMaster("local[4]") \
+        .set("spark.executor.cores", "2")
+    spark5 = SparkContext(conf=conf5)
+    t5 = time.time()
+    tmalign(spark5, "OriginalPDBs", "OriginalPDBscopy")
+    t5c = time.time()
+
+    conf6 = SparkConf().setAppName("MyApp").setMaster("local[8]") \
+        .set("spark.executor.cores", "1")
+    spark6 = SparkContext(conf=conf5)
+    t6 = time.time()
+    tmalign(spark6, "OriginalPDBs", "OriginalPDBscopy")
+    t6c = time.time()
+
+
+    times = [(t1c-t1), (t2c-t2), (t3c-t3), (t4c-t4), (t5c-t5), (t6c-t6)]
+    configurations = ["Config 1", "Config 2", "Config 3", "Config 4", "Config 5", "Config 6"]
+
+    plt.plot(configurations, times, linewidth=2, color="#1f77b4")
+    plt.ylabel("Time (seconds)", fontsize=12)
+    plt.xlabel("Configuration", fontsize=12)
+    plt.title("Line Plot of Execution Time by Configuration", fontsize=14)
+    plt.xticks(fontsize=10, rotation=45)
+    plt.yticks(fontsize=10)
+    plt.grid(axis="y", linestyle=":", alpha=0.7)
+    plt.show()
+bench()
